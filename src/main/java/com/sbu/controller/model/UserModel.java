856afd3387ae2170b5473b4b0a6f6b4f5a4dbd13
Soraya73 <sandy.chimney@gmail.com>
2017-05-30 17:15:47 +0430

    Add files via upload

---
+++ src/main/java/com/sbu/controller/model/UserModel.java
@@ -0,0 +1,119 @@
+
+package com.sbu.controller.model;
+
+public class UserModel {
+
+    private Integer id;
+    private String password;
+    private Integer urole;
+    private String fName;
+    private String lName;
+    private Integer code;
+    private String gender;
+    private String birthDate;
+    private String nationalCode;
+    private String email;
+    private String phone;
+    private String address;
+
+    public UserModel() {
+    }
+    
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUrole() {
+        return urole;
+    }
+
+    public void setUrole(Integer urole) {
+        this.urole = urole;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }  
+}
