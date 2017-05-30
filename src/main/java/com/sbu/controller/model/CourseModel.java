128c28d6e18fa5510f2b82efd89093ef6bc7092a
Soraya73 <sandy.chimney@gmail.com>
2017-05-30 17:18:48 +0430

    Add files via upload

---
+++ src/main/java/com/sbu/controller/model/CourseModel.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbu.controller.model;
+
+/**
+ *
+ * @author ghazalhakimifard
+ */
+public class CourseModel {
+    Integer id;
+    Integer code;
+    String name;
+    Integer unit;
+    String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Integer unit) {
+        this.unit = unit;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    
+    
+}
