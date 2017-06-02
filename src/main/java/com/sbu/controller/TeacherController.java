3d9d34221562954fe18ca9cd8c0c93a2e85578c7
melikaSaraf <melikasarraf@gmail.com>
2017-06-02 22:22:45 +0430

    Update TeacherController.java

--- src/main/java/com/sbu/controller/TeacherController.java
+++ src/main/java/com/sbu/controller/TeacherController.java
@@ -5,8 +5,14 @@
  */
 package com.sbu.controller;
 import com.dao.entity.Lesson;
+import com.dao.entity.Student;
+import com.dao.entity.Studenttermlessonteacher;
 import com.dao.entity.Term;
+import com.dao.entity.User;
 import com.sbu.controller.model.CourseModel;
+import com.sbu.controller.model.StudentModel;
+import com.sbu.controller.model.StudenttermlessonteacherModel;
+import com.sbu.controller.model.UserModel;
 import com.sbu.service.impl.TeacherManagerImpl;
 import com.sbu.service.impl.UserManagerImpl;
 import java.util.ArrayList;
@@ -16,8 +22,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -31,7 +39,8 @@ public class TeacherController {
     @Autowired
     public TeacherManagerImpl teacherManagerImpl;
     
-
+//    @Autowired
+//    public StudenttermlessonteacherModel  sttltmodel;
     
  
    ///////////////////////////////////////////
@@ -53,37 +62,6 @@ public class TeacherController {
               return "redirect:/login";
 	}
               
-                
-     //insertCourse
-//   	@RequestMapping(value = "insertCourse", method = RequestMethod.POST)
-//	public String insertCourse(HttpSession session , @ModelAttribute("SpringWeb")CourseModel course , Model model ) {
-//                String userCode = ""; 
-//                System.out.println("inside insertCourse function");
-//             if(session.getAttribute("userCode") != null)
-//              {
-//                userCode = session.getAttribute("userCode").toString();
-//                 //if(Integer.parseInt(userCode) == 1)
-//                {         
-//                    System.out.println("codeeee" + course.getCode());
-//                    System.out.println("nameeee" + course.getName());
-//                    System.out.println("typeeeee" + course.getType());
-//                    System.out.println("unitttt" + course.getUnit());
-//
-//                    CourseModel tempCourse = new CourseModel();
-//                    tempCourse.setCode(course.getCode());
-//                    tempCourse.setName(course.getName());   
-//                    tempCourse.setType(course.getType());
-//                    tempCourse.setUnit(course.getUnit());
-//                   
-//                    headOfDepartmentManagerImpl.insertNewCourse(tempCourse);
-//
-//                    return "/newOrEditCourse";              
-//                }
-//               
-//              }
-//             else
-//                 return "redirect:/login";
-//	}
         
         //////////////////////////////////
 
@@ -100,7 +78,7 @@ public class TeacherController {
         @RequestMapping(value = "showCourse", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody List<CourseModel> showCourse(HttpSession session , String termid , Model model ) {
                 String userCode = ""; 
-                System.out.print("termid here"+termid);
+               // System.out.print("termid here"+termid);
                 userCode = session.getAttribute("userCode").toString();
                 Integer userCodeInt = Integer.parseInt(userCode);
                 Integer termCodeInt =  Integer.parseInt(termid);
@@ -117,8 +95,7 @@ public class TeacherController {
                     temp.setName(l.getName());
                     temp.setType(l.getType());
                     temp.setUnit(l.getUnit());
-                    courses.add(temp);
-                    
+                    courses.add(temp);                   
                 }
                                
 
@@ -127,16 +104,49 @@ public class TeacherController {
                 
          ///////////////////////////////////////////////
         
-        //update
+        //show students
+          @RequestMapping(value = "showStudents", method = RequestMethod.POST , produces = "application/json")
+	public  @ResponseBody List<UserModel> showStudents(HttpSession session  , String tid , String thisLessonId , Model model ) {
+            String teacherCode = "";  
+            teacherCode = session.getAttribute("userCode").toString();
+            Integer teacherCodeInt = Integer.parseInt(teacherCode);
+            Integer termCodeInt =  Integer.parseInt(tid);
+            Integer thisLessonIdInt =  Integer.parseInt(thisLessonId);  
+            List<User> stltlist = teacherManagerImpl.findStudentTermLessonTeacher(teacherCodeInt ,termCodeInt ,thisLessonIdInt);
+            
+            List<UserModel> studentlist =  new ArrayList<UserModel>() ; 
+            
+            for(int i=0; i<stltlist.size(); i++)
+                {
+                    User l = stltlist.get(i);
+                    UserModel temp = new UserModel();
+                    temp.setId(l.getId());
+                    temp.setCode(l.getCode());
+                    temp.setfName(l.getFname());
+                    temp.setlName(l.getLname());
+                    
+                   // temp.setSupervisorId(l.getSupervisorid().getCode());
+                    studentlist.add(temp);                    
+                }
+           // String t = studentlist.get(0).getUserCode().toString();
+            //System.out.println("afterr  controler : " + t );
+
+              return studentlist;
+        }
+                     
+        ///////////////////////////////////////////////
         
-         //edit profile
-//   	@RequestMapping(value = "/editCourse", method = RequestMethod.POST)
-//	public @ResponseBody boolean editCourse(@RequestBody CourseModel course) {
-//                
-//             System.out.println("com.sbu.controller.ManagerController.editCourse()");
-//             headOfDepartmentManagerImpl.updateCourseByCode(course);
-//            return true;
-//        }
-//        
+         @RequestMapping(value = "InsertMark", method = RequestMethod.POST ) // , produces = "application/json")
+        // @ResponseBody
+	public  void InsertMark(HttpSession session , @RequestParam(value = "termid[]", required = false) String[] termid ,
+                @RequestParam(value = "thisLessonId[]", required = false) String[] thisLessonId ,
+                @RequestParam(value = "mark[]", required = false) String[] mark ,
+                @RequestParam(value = "studentId[]", required = false) String[] studentId  , Model model )
+        {
+          
+            System.out.println("Insert Mark controller : " + termid[0]  +  thisLessonId[0] + studentId[0] + mark[0]);
+        }
+        
      
 }
+
