8827eb2c615df5262b0c2f3a547cbee2fdf498a4
Soraya73 <sandy.chimney@gmail.com>
2017-06-14 10:14:02 +0430

    Add files via upload

--- src/main/java/com/sbu/service/impl/HeadOfDepartmentManagerImpl.java
+++ src/main/java/com/sbu/service/impl/HeadOfDepartmentManagerImpl.java
@@ -1,5 +1,9 @@
 package com.sbu.service.impl;
 
+import com.dao.entity.Lesson;
+import com.dao.entity.Studenttermlessonteacher;
+import com.dao.entity.Term;
+import com.dao.entity.Teacher;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +16,16 @@ import com.sbu.controller.model.CourseModel;
 import com.sbu.controller.model.TermModel;
 import com.sbu.controller.model.UserModel;
 import com.sbu.dao.impl.HeadOfDepartmentDAOImpl;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 
 @Service
 public class HeadOfDepartmentManagerImpl /*implements UserManager*/ {
 
+  
+
+    
     public HeadOfDepartmentManagerImpl() {
     }
 
@@ -44,6 +53,41 @@ public class HeadOfDepartmentManagerImpl /*implements UserManager*/ {
        // System.out.println("com.sbu.service.impl.HeadOfDepartmentManagerImpl.updateCourseByCode()");
         headOfDepartmentDAOImpl.updateCourseByCode(course);
     }
+    
+   
+    public List<Term> findTerm ()
+    {
+        return headOfDepartmentDAOImpl.findTerm();
+
+    }
+    
+    public List<Teacher> findTeacher ()
+    {
+        return headOfDepartmentDAOImpl.findTeachers();
+
+    }
+    
+     public List<Lesson> findTermLessonTeacher(Integer termCode , Integer userCode)
+     {
+         return headOfDepartmentDAOImpl.findTermLessonTeacher(termCode , userCode);
+     }
+    
+     
+      public List<Studenttermlessonteacher> findStudentTermLessonTeacher(Integer teacherCodeInt ,Integer termCodeInt ,Integer thisLessonIdInt)
+     {
+         //System.out.println(" Manager resulttttt " );
+         return  headOfDepartmentDAOImpl.findStudentTermLessonTeacher(teacherCodeInt ,termCodeInt ,thisLessonIdInt);
+         
+     }
+      
+       public void insertMark2(String[] termid, String[] thisLessonId, String[] mark, String[] studentId , Integer teacherId) {
+      
+        headOfDepartmentDAOImpl.insertMark2(termid, thisLessonId, mark, studentId , teacherId);
+       
+    }
+      
+      
+     
 }
 
 
