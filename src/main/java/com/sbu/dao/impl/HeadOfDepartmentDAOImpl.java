17cad86ce110f8e62308086750408be5465a5c05
Soraya73 <sandy.chimney@gmail.com>
2017-06-14 10:13:02 +0430

    Add files via upload

--- src/main/java/com/sbu/dao/impl/HeadOfDepartmentDAOImpl.java
+++ src/main/java/com/sbu/dao/impl/HeadOfDepartmentDAOImpl.java
@@ -1,11 +1,14 @@
 package com.sbu.dao.impl;
 
 import com.dao.entity.Lesson;
+import com.dao.entity.Studenttermlessonteacher;
 import com.dao.entity.Term;
+import com.dao.entity.Teacher;
 import com.dao.entity.User;
 import com.sbu.controller.model.CourseModel;
 import com.sbu.controller.model.TermModel;
 import com.sbu.controller.model.UserModel;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +17,7 @@ import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -106,5 +110,94 @@ public class HeadOfDepartmentDAOImpl /*implements UserDAO*/ {
             
         }
     }
+    
+    
+     public List<Term> findTerm()
+    {
+        return entityManager.createNamedQuery("Term.findAll").getResultList();
+       
+    }
+     
+      public List<Teacher> findTeachers()
+    {
+        return entityManager.createNamedQuery("Teacher.findAll").getResultList();
+       
+    }
+     
+     @Transactional
+    public List<Lesson> findTermLessonTeacher(Integer termCode , Integer userCode)
+    {
+            Integer lessonid = 3; 
+            Query qu = entityManager.createQuery("SELECT l FROM  Lesson l WHERE  l.code IN "
+            + "( SELECT t FROM Termlessonteacher t WHERE t.teacherid.code = :teacherid  AND  t.termid.code = :termid)");
+            //Query qu = entityManager.createQuery("SELECT t FROM Termlessonteacher t  WHERE t.teacherid.code = :teacherid  AND  t.termid.code = :termid");
+            qu.setParameter("teacherid" , userCode);
+            qu.setParameter("termid" , termCode);
+            List<Lesson> lessonlist =  qu.getResultList();
+            
+            //System.out.println("ll issssssssss"); //+ res.toString() );
+         
+         return lessonlist;
+    }
+    
+    
+        
+    @Transactional
+
+    public List<Studenttermlessonteacher> findStudentTermLessonTeacher(Integer teacherCodeInt ,Integer termCodeInt ,Integer thisLessonIdInt)
+    {
+        Query qu1;
+        qu1 = entityManager.createQuery("SELECT stlt FROM Studenttermlessonteacher stlt WHERE stlt.termlessonteacherid.id IN  " 
+
+                + "(SELECT t FROM Termlessonteacher t"
+                + " WHERE  t.termid.code = :termid AND t.teacherid.code = :teacherid AND t.lessonid.code = :lessonid)" );      
+        qu1.setParameter("termid", termCodeInt );
+        qu1.setParameter("teacherid", teacherCodeInt );
+        qu1.setParameter("lessonid", thisLessonIdInt );
+        
+        List<Studenttermlessonteacher> result = qu1.getResultList();
+       
+        return result;
+    }
+    
+    
+     @Transactional
+
+        public void insertMark2(String[] termId, String[] LessonId, String[] mark, String[] studentId , Integer teacherid)
+     {
+         if(mark.length != 0)
+         {
+            Integer termid = Integer.parseInt(termId[0]);
+            Integer lessonid = Integer.parseInt(LessonId[0]);
+
+            Query qu1 , qu2;
+            qu1 = entityManager.createQuery("SELECT t.id FROM Termlessonteacher t WHERE t.termid.code = :termId  AND t.teacherid.code = :teacherId AND t.lessonid.code = :lessonId ");
+            qu1.setParameter("termId", termid);
+            qu1.setParameter("teacherId", teacherid);
+            qu1.setParameter("lessonId", lessonid);
+
+            List<Integer> resultId = qu1.getResultList();
+            Integer termlessonteacherid = resultId.get(0); // Integer.parseInt(str);
+
+            
+           qu2 = entityManager.createQuery("SELECT s FROM Studenttermlessonteacher s WHERE s.termlessonteacherid.id = :termlessonteacherid");
+           qu2.setParameter("termlessonteacherid", termlessonteacherid);           
+           List<Studenttermlessonteacher> selectResult = qu2.getResultList();   // get all students having this lesson with this teacher in yhis term
+
+           
+           List<String> studentIdList = Arrays.asList(studentId);
+           for( int i=0; i<studentId.length; i++)
+           {
+               int index = studentIdList.indexOf(selectResult.get(i).getStudentid().getCode().toString());
+               int markInt = Integer.parseInt(mark[index]);
+               selectResult.get(i).setMark(markInt);
+           }
+          // System.out.println("befor bulkkkkkkkkkk" +  selectResult.get(0).getMark() );
+         
+
+        }
+     }
+
+    
 
 }
